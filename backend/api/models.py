8a3e9f1b533370182e54d3a2874ac52a57b543cb
Melaine Euzenat <melaine.euzenat@gmail.com>
2020-06-06 12:07:18 +0200

    Basic model and API structure

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+
+    path = models.CharField(max_length=256)
+    name = models.CharField(max_length=100)
+    date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f'<Image: {self.name}>'
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Image
+from .serializers import ImageSerializer
+
+
+class ImageViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Images to be viewed or edited.
+    """
+    queryset = Image.objects.all().order_by('-date')
+    serializer_class = ImageSerializer
