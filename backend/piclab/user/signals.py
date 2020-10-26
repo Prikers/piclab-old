f89ef4d8e04b2f54debd19062773e82775a49cc8
Melaine Euzenat <melaine.euzenat@gmail.com>
2020-10-27 00:08:53 +0100

    Backend - Create default project at User creation

--- backend/piclab/user/signals.py
+++ backend/piclab/user/signals.py
@@ -1,17 +1,20 @@
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
 from .models import Profile
+from piclab.api.models import Project
 
 User = get_user_model()
 
-
+# Create a Profile and a default Project after User creation
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
+def create_profile_and_project(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
-
+        project = Project.objects.create(owner=instance, name='Gallery')
+        Profile.objects.create(user=instance, current_project=project)
 
 @receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
+def save_profile_and_project(sender, instance, **kwargs):
     instance.profile.save()
+    instance.profile.current_project.save()
