7c2f1250896aedd1885a5df344a69abf3079ad23
Melaine Euzenat <melaine.euzenat@gmail.com>
2020-11-29 21:49:02 +0100

    Backend - Install Django-filters

--- backend/piclab/settings/base.py
+++ backend/piclab/settings/base.py
@@ -14,6 +14,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'corsheaders',
+    'django_filters',
 
     'piclab.api.apps.ApiConfig',
     'piclab.user.apps.UserConfig',
@@ -84,6 +85,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     # 'PAGE_SIZE': 100,
 }
